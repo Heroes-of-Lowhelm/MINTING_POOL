14cb73d0e1f44eb96ec043fe283896e9e2940864
asatriangrish <68015194+asatriangrish@users.noreply.github.com>
2022-03-30 16:52:41 +0400

    feat: deploy script

--- Deploy/deploy.js
+++ Deploy/deploy.js
@@ -1,9 +1,1 @@
-function readContractCodeByPath (fullContractPath, ext = '.scilla')
-{
-    const { sep, resolve } = require('path');
-    const { existsSync, readFileSync } = require('fs');
-    if (!existsSync(fullContractPath)) {
-        throw new Error('Contract not found at path: ' + fullContractPath);
-    }
-    return readFileSync(fullContractPath).toString();
-}
+
